6158019efbe7c2b0f6ff7c09966d1ce85d85385f
mokassino <54906285+mokassino@users.noreply.github.com>
2022-04-21 17:00:01 +0200

    Esercizio laboratorio 1 fatti

--- util.hpp
+++ util.hpp
@@ -5,7 +5,59 @@
 using namespace cv;
 using namespace std;
 
-//copyMakeBorder ma rifatta a mano
+int calc_media(Mat &p, int row, int col, int chan, int kernel){
+    vector<int> vec;
+    int media = 0;
+
+    vec.push_back( (int) p.at<uchar>(row-1,col,chan) );
+    vec.push_back( (int) p.at<uchar>(row-1,col-1, chan) );
+    vec.push_back( (int) p.at<uchar>(row-1,col+1, chan) );
+
+    vec.push_back( (int) p.at<uchar>(row+1,col-1,chan) );
+    vec.push_back( (int) p.at<uchar>(row+1,col+1, chan) );
+    vec.push_back( (int) p.at<uchar>(row+1,col, chan) );
+
+    vec.push_back( (int) p.at<uchar>(row,col-1,chan) );
+    vec.push_back( (int) p.at<uchar>(row,col, chan) );
+    vec.push_back( (int) p.at<uchar>(row,col+1, chan) );
+
+    
+    //somma tutti gli elementi del vettore tramite Iterator
+    for(std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it){
+        media += *it;
+    }
+
+    media = media/kernel;
+
+    //Controlla se la media è > 255, mettila a 255
+    if (media > 255){
+        media = 255;
+    }
+    
+    return media;
+    
+}
+
+Mat padding(Mat &p, int kernel){
+    int nRows = p.rows;
+    int nCols = p.cols;
+    int nChan = p.channels();
+
+    int i,j,c;
+
+     for (i = 1; i < nRows; ++i){
+        for (j = 1; j < nCols; ++j){
+            for (c = 0; c < nChan; c++){
+                
+                int media = calc_media(p, i, j , c, kernel*kernel);
+                p.at<uchar>(i,j,c) = (uchar) media;
+            }
+           
+        }
+    }
+
+}
+
 Mat makeBorder(Mat& M, int extra){
 
     CV_Assert(M.depth() == CV_8U);
@@ -15,6 +67,7 @@ Mat makeBorder(Mat& M, int extra){
     int nRows = M.rows;
     int nCols = M.cols;
     int nChan = M.channels();
+
     Mat base = cv::Mat(nRows+extra, nCols+extra, CV_8U, cv::Scalar(255,255,255));
 
 
